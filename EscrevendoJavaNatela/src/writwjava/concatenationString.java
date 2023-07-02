157366523eb994c5f524996579b85404217f18d5
edudias1972 <edudias1972@gmail.com>
2023-07-02 16:04:31 -0300

    Concatenando string + string

---
+++ EscrevendoJavaNatela/src/writwjava/concatenationString.java
@@ -0,0 +1,18 @@
+package writwjava;
+
+public class concatenationString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated  method stub
+		
+		String x = "Hello cats  ";
+		// concatenando valor + valor 
+		System.out.println(x + "and dogs ");
+		
+		// troca o valor da concatenação 
+		x += " and puppies ";
+		System.out.println(x);
+		
+	}
+
+}
