71515e1e82f49cfea98becdd14465e9cc8928b6b
edudias1972 <edudias1972@gmail.com>
2023-07-02 15:21:27 -0300

    Tipos Primitivos

---
+++ EscrevendoJavaNatela/src/writwjava/tiposdeVariaveis.java
@@ -0,0 +1,43 @@
+package writwjava;
+
+public class tiposdeVariaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Tipos de variaveis 
+		
+       System.out.println("Tipos de variaveis");
+       
+       boolean isPizzaDelicious = true;
+       byte b = 'C';
+       char c = 'Z';
+       short s = -32768;
+       int i = 4343434;
+       // log l = 50L;
+       
+       float f = 20.5f;
+       double d = 20.5;
+       float ff = 20.5f;
+       double dd = 20.5f;
+       
+       System.out.println(isPizzaDelicious);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(s);
+       System.out.println(i);
+       // System.out.println(l);
+       System.out.println(f);
+       System.out.println(d);
+       
+       System.out.println(ff);
+       System.out.println(dd);
+       
+       float x = (float)(dd /s);
+       
+       System.out.println(x);
+       
+	}
+
+}
+
+// boolean , byte , char , short , int , long , float and double
