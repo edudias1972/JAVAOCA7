003513ebdaa868f26876a7fc9dd52129117656ee
edudias1972 <edudias1972@gmail.com>
2023-07-09 14:06:19 -0300

    Escrevendo sysout e utilzando a tecla CTRL mais a barra de espaço

---
+++ EscrevendoJavaNatela/src/writwjava/HelloJAVA.java
@@ -0,0 +1,11 @@
+package writwjava;
+
+public class HelloJAVA {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    // Dica - > Escrevendo sysout e utilzando a tecla CTRL mais a barra de espaço
+    System.out.println("Hello Java");
+  }
+
+}
