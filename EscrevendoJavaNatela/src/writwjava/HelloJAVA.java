68b92ac8130bc3486fcc80077799a5302d0b5e74
edudias1972 <edudias1972@gmail.com>
2023-07-09 14:23:23 -0300

    Variaveis e tipos

--- EscrevendoJavaNatela/src/writwjava/HelloJAVA.java
+++ EscrevendoJavaNatela/src/writwjava/HelloJAVA.java
@@ -5,7 +5,7 @@ public class HelloJAVA {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     // Dica - > Escrevendo sysout e utilzando a tecla CTRL mais a barra de espaço
-    System.out.println("Hello Java");
+    System.out.println("Hello Java! ");
   }
 
 }
---
+++ EscrevendoJavaNatela/src/writwjava/VariaveisTipos.java
@@ -0,0 +1,19 @@
+package writwjava;
+
+public class VariaveisTipos {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int idade = 10;
+    double preco = 10.5;
+    char sexo = 'M';
+    boolean temFilhos = true;
+
+    System.out.println("Tipo Integer : " + idade);
+    System.out.println("Tipo Double :" + preco);
+    System.out.println("Tipo Char :" + sexo);
+    System.out.println("Tipo  Bollean : " + temFilhos);
+
+  }
+
+}
